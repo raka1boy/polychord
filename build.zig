88a35b86f8bf02238125ca94d40bb745868274f8
ДИТО <dito@mabiu.local>
2025-04-16 04:14:52 +0300

    zig build oohh uhhhhh

---
+++ build.zig
@@ -0,0 +1,58 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const lib_mod = b.createModule(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe_mod.addImport("polychord_lib", lib_mod);
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "polychord",
+        .root_module = lib_mod,
+    });
+
+    b.installArtifact(lib);
+    const exe = b.addExecutable(.{
+        .name = "polychord",
+        .root_module = exe_mod,
+    });
+    const zaudio = b.dependency("zig-portaudio", .{ .target = target, .optimize = optimize });
+    var it = zaudio.builder.modules.iterator();
+    while (it.next()) |mod| {
+        std.debug.print("ssss{any}\n", .{mod.value_ptr.*.export_symbol_names});
+    }
+    //exe.root_module.addImport("zaudio", zaudio.module("zaudio"));
+    // b.dependency("zaudio", .{});
+    b.installArtifact(exe);
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+    const lib_unit_tests = b.addTest(.{
+        .root_module = lib_mod,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    const exe_unit_tests = b.addTest(.{
+        .root_module = exe_mod,
+    });
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&run_exe_unit_tests.step);
+}
---
+++ src/synth.zig
@@ -0,0 +1,7 @@
+const Harmonic = struct {
+    amp: f16,
+    freq: u16,
+    phase: f16,
+};
+
+const std = @import("std");
