f002e8fd6eeeb325c0eed8710124707857b4c72e
ДИТО <dito@mabiu.local>
2025-04-19 15:57:32 +0300

    dsads

--- src/synth.zig
+++ src/synth.zig
@@ -3,5 +3,33 @@ const Harmonic = struct {
     freq: u16,
     phase: f16,
 };
+const Synthesizer = struct {
+    harmonics: std.ArrayList(Harmonic),
+    sample_rate: u32,
+    global_smoothing: f16,
+    global_amp: f16,
 
+    pub fn init(
+        alloc: std.mem.Allocator,
+        sample_rate: u32,
+        global_smoothing: f16,
+        global_amp: f16,
+    ) !Synthesizer {
+        return .{
+            .harmonics = try .init(alloc),
+            .sample_rate = sample_rate,
+            .global_smoothing = global_smoothing,
+            .global_amp = global_amp,
+        };
+    }
+    pub fn deinit(self: *Synthesizer) void {
+        self.harmonics.deinit();
+    }
+    pub fn initDefault(alloc: std.mem.Allocator) !Synthesizer {
+        return try init(alloc, 48000, 0, 1);
+    }
+};
 const std = @import("std");
+const c = @cImport({
+    @cInclude("portaudio.h");
+});
